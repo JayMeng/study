3188ed37461a758039336868908f401ebe279ceb
1029685278@qq.com <1029685278@qq.com>
2020-03-16 16:59:45 +0800

    单链表

--- base-java/src/main/java/com/jay/datastructure/CircleArrayQueueDemo.java
+++ base-java/src/main/java/com/jay/datastructure/CircleArrayQueueDemo.java
@@ -18,44 +18,52 @@ class CircleArrayQueue {
     private int front;
     private int rear;
     private int[] queue;
+
     public CircleArrayQueue(int maxSize) {
-        this.maxSize = maxSize;
+        this.maxSize = maxSize; //队列的长度，比实际队列应用空间大1，比如实际new int[10]，其实应该队列只能存放到a[8]，只能放9个元素
         queue = new int[maxSize];
         front = 0;//    此处的front指向队列的第一个元素
-        rear = 0;//     此处的rear指向队列最后一个元素的后一个元素（环形队列，初始直接指向头，尾部的下一个就是头）
+        rear = 0;//     指向队列最后一个元素的后一个位置，因此需要队列预留一个空间，所以呢数组最大空间要比实际应用大。
     }
+
     public Boolean isFull() {
         return (rear + 1) % maxSize == front;
-    }
+    } //    这里需要注意的是队列预留一个空间， 所以实际满的对列是maxsize-1,此时只要判断rear是否在预留位置上
+
     public Boolean isEmpty() {
-        return front == rear;
+        return front == rear;   //front每次取完后+1,rear又代表最后一个元素的后一个位置。相等即代表取完了
     }
+
     public void addQueue(int n) {
         if (isFull()) {
             System.out.println("队列已满");
             return;
         }
         queue[rear] = n;
-        rear = (rear + 1 ) % maxSize;
+        rear = (rear + 1) % maxSize;
     }
+
     public int getQueue() {
         if (isEmpty()) {
             System.out.println("队列已空");
             return -9999;
         }
         int n = queue[front];
-        front = (front + 1 ) % maxSize;
+        front = (front + 1) % maxSize;
         return n;
     }
+
     public void printQueue() {
         for (int i = front; i < front + size(); i++) {
-            System.out.println("queue[" + i % maxSize + "] = " + queue[i % maxSize ]);
+            System.out.println("queue[" + i % maxSize + "] = " + queue[i % maxSize]);
         }
     }
+
     public int size() {
         return (rear + maxSize - front) % maxSize;
     }
-    public int showHeadQueue(){
+
+    public int showHeadQueue() {
         return queue[front];
     }
 }
---
+++ base-java/src/main/java/com/jay/reflection/Ceshi.java
@@ -0,0 +1,9 @@
+package com.jay.reflection;
+
+/**
+ * @Desc:
+ * @Author: 1029685278@qq.com
+ * @Date: 2020-01-12 19:30
+ **/
+public class Ceshi {
+}
