45bd102741190eaf507e7313f1e1e2ae15fa36c8
1029685278@qq.com <1029685278@qq.com>
2020-03-18 16:59:29 +0800

    约瑟夫问题

---
+++ base-java/src/main/java/com/jay/datastructure/linkedlist/Josephu.java
@@ -0,0 +1,56 @@
+package com.jay.datastructure.linkedlist;
+
+/**
+ * @author jay
+ * @create 2020-03-18 16:23
+ **/
+
+public class Josephu {
+    //  经典的约瑟夫问题（丢手绢）
+    //  设定编号为1，2，3 ...n 的 n 个人围坐一圈，约定 编号为 k （1<=k<=n）人从1开始报数，数到 m 的人出列，他的下一位再从1开始，数到 m 的人 又出列
+    //  以此类推，知道所有人都出列为止，由此 顺序输出 出列人的 编号
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+class CircleSingleLinkedList{
+
+
+
+
+
+
+
+
+}
+
+
+
+class Boy {
+    private int no;
+
+    private Boy next;
+
+    public Boy(int no) {
+        this.no = no;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public Boy getNext() {
+        return next;
+    }
+
+    public void setNext(Boy next) {
+        this.next = next;
+    }
+}
